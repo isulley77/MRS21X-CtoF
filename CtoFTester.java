58ae2b4c1dd1c766d4260d816958b7392b27bdf3
isulley77 <isulley@stuy.edu>
2018-09-20 06:47:40 -0400

    needs debugging

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,17 @@
- 
+ public static int celciusToFahrenheit(int tempC){
+
+   return tempC * 9/5 +32
+
+ }
+
+ public static int fahrenheitToCelcius (int tempF){
+
+   return (TempF - 32) * 5/9
+
+ }
+
+ public static int main(int temp){
+
+   return celciusToFahrenheit(temp);
+   return fahrenheitToCelcius(temp);
+ }
